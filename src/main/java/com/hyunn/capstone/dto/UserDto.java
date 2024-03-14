2282d264dad33bac27c430ca0e914d08d71dac9b
HyunTae Jo <hyuntae9912@naver.com>
2024-03-14 11:16:51 +0900

    UserDto 생성 및 유효성 검사 추가

---
+++ src/main/java/com/hyunn/capstone/dto/UserDto.java
@@ -0,0 +1,26 @@
+package com.hyunn.capstone.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class UserDto {
+  @NotBlank(message = "핸드폰 번호를 입력해주세요.")
+  @Pattern(regexp = "^\\d{11}$", message = "핸드폰 번호는 11자리여야 합니다.")
+  private String phone;
+
+  @NotBlank(message = "비밀번호를 입력해주세요.")
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+      message = "비밀번호는 영문자, 숫자, 특수문자를 모두 포함하여 8자리 이상이어야 합니다.")
+  private String password;
+
+  public UserDto(String phone, String password) {
+    this.phone = phone;
+    this.password = password;
+  }
+
+  public static UserDto create(String phone, String password) {
+    return new UserDto(phone, password);
+  }
+}
