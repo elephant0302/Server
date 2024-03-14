86ae77085ad5742d23c40c824a04e32d1d07b185
HyunTae Jo <hyuntae9912@naver.com>
2024-03-14 17:02:52 +0900

    dto 수정

---
+++ src/main/java/com/hyunn/capstone/dto/Request/UserDeleteRequest.java
@@ -0,0 +1,27 @@
+package com.hyunn.capstone.dto.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class UserDeleteRequest {
+  @NotBlank(message = "핸드폰 번호를 입력해주세요.")
+  @Pattern(regexp = "^\\d{11}$", message = "핸드폰 번호는 11자리여야 합니다.")
+  private String phone;
+
+  @NotBlank(message = "비밀번호를 입력해주세요.")
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+      message = "비밀번호는 영문자, 숫자, 특수문자를 모두 포함하여 8자리 이상이어야 합니다.")
+  private String password;
+
+
+  public UserDeleteRequest(String phone, String password) {
+    this.phone = phone;
+    this.password = password;
+  }
+
+  public static UserDeleteRequest create (String phone, String password) {
+    return new UserDeleteRequest(phone, password);
+  }
+}
