c7bd8cf03a48b24be4e4b00247542c0f56e8c7e3
HyunTae Jo <hyuntae9912@naver.com>
2024-03-14 12:35:16 +0900

    1. User에 주소와 상태 추가 -> UserDto 변경 2. 회원 생성, 삭제, 복구를 위한 Setter 추가 3. userRepo 수정

--- src/main/java/com/hyunn/capstone/dto/UserDto.java
+++ src/main/java/com/hyunn/capstone/dto/UserDto.java
@@ -15,12 +15,16 @@ public class UserDto {
       message = "비밀번호는 영문자, 숫자, 특수문자를 모두 포함하여 8자리 이상이어야 합니다.")
   private String password;
 
-  public UserDto(String phone, String password) {
+  @NotBlank(message = "도로명 주소를 입력해주세요.")
+  private String address;
+
+  public UserDto(String phone, String password, String address) {
     this.phone = phone;
     this.password = password;
+    this.address = address;
   }
 
-  public static UserDto create(String phone, String password) {
-    return new UserDto(phone, password);
+  public static UserDto create(String phone, String password, String address) {
+    return new UserDto(phone, password, address);
   }
 }
--- src/main/java/com/hyunn/capstone/repository/UserJpaRepository.java
+++ src/main/java/com/hyunn/capstone/repository/UserJpaRepository.java
@@ -1,9 +1,13 @@
 package com.hyunn.capstone.repository;
 
 import com.hyunn.capstone.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 
+  Optional<User> findUserByPhoneAndPassword(String phone, String Password);
+
+  boolean existsUserByPhoneAndPassword(String phone, String password);
 
 }
