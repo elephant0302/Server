5b29ebd060ae1082d578b4b7862d13ae462173fa
HyunTae Jo <hyuntae9912@naver.com>
2024-03-15 17:14:09 +0900

    유저별 이미지 기록 확인 기능

--- src/main/java/com/hyunn/capstone/service/UserService.java
+++ src/main/java/com/hyunn/capstone/service/UserService.java
@@ -1,12 +1,17 @@
 package com.hyunn.capstone.service;
 
-import com.hyunn.capstone.dto.Request.UserDeleteRequest;
+import com.hyunn.capstone.dto.Request.UserRequest;
+import com.hyunn.capstone.dto.Response.ImageResponse;
 import com.hyunn.capstone.dto.UserDto;
+import com.hyunn.capstone.entity.Image;
 import com.hyunn.capstone.entity.User;
 import com.hyunn.capstone.exception.UserAlreadyExistException;
 import com.hyunn.capstone.exception.UserNotFoundException;
+import com.hyunn.capstone.repository.ImageJpaRepository;
 import com.hyunn.capstone.repository.UserJpaRepository;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +20,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
   private final UserJpaRepository userJpaRepository;
+  private final ImageJpaRepository imageJpaRepository;
 
+  /**
+   * 로그인
+   */
   public UserDto login(UserDto userDto) {
     // userDto에서 필요 정보 추출
     String phone = userDto.getPhone();
@@ -44,6 +53,9 @@ public class UserService {
     return UserDto.create(newUser.getPhone(), newUser.getPassword(), newUser.getAddress());
   }
 
+  /**
+   * 휴면 계정 복구
+   */
   public UserDto rollBack(Long userId) {
     Optional<User> user = Optional.ofNullable(userJpaRepository.findById(userId)
         .orElseThrow(() -> new UserNotFoundException("유저 정보를 가져오지 못했습니다.")));
@@ -53,9 +65,12 @@ public class UserService {
     return UserDto.create(existUser.getPhone(), existUser.getPassword(), existUser.getAddress());
   }
 
-  public void deleteUser(UserDeleteRequest userDeleteRequest) {
-    String phone = userDeleteRequest.getPhone();
-    String password = userDeleteRequest.getPassword();
+  /**
+   * 계정 탈퇴 (휴면 계정으로 변경) -> 한달 후 삭제하도록 로직 찾아보자!
+   */
+  public void deleteUser(UserRequest userRequest) {
+    String phone = userRequest.getPhone();
+    String password = userRequest.getPassword();
 
     // 계정이 없는 경우
     Optional<User> existUser = Optional.ofNullable(
@@ -67,5 +82,30 @@ public class UserService {
     userJpaRepository.save(deleteUser);
   }
 
+  /**
+   * 유저 정보로 관련 이미지 리스트로 반환하기
+   */
+  public List<ImageResponse> findImagesByUser(UserRequest userRequest) {
+    String phone = userRequest.getPhone();
+    String password = userRequest.getPassword();
+
+    Optional<User> existUser = Optional.ofNullable(
+        userJpaRepository.findUserByPhoneAndPassword(phone, password)
+            .orElseThrow(() -> new UserNotFoundException("유저 정보를 가져오지 못했습니다.")));
+
+    // 휴면 계정이 존재하는 경우, 없는 걸로 처리
+    if (!existUser.get().getStatus()) {
+      throw new UserAlreadyExistException("유저 정보를 가져오지 못했습니다.");
+    }
+
+    List<Image> images = imageJpaRepository.findAllByUser(existUser);
+
+    // Entity에서 Dto로 변형
+    List<ImageResponse> imageResponses = images.stream()
+        .map(image -> new ImageResponse(image.getThreeDimension(), image.getKeyWord()))
+        .collect(Collectors.toList());
+
+    return imageResponses;
+  }
 
 }
