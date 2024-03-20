d34fb211e4b8eeb5e1c9a3457197053637d7d9cc
HyunTae Jo <hyuntae9912@naver.com>
2024-03-20 20:50:50 +0900

    응답값 수정

--- src/main/java/com/hyunn/capstone/controller/ImageController.java
+++ src/main/java/com/hyunn/capstone/controller/ImageController.java
@@ -2,19 +2,19 @@ package com.hyunn.capstone.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hyunn.capstone.dto.Request.ImageRequest;
+import com.hyunn.capstone.dto.Response.ApiStandardResponse;
 import com.hyunn.capstone.dto.Response.ThreeDimensionCreateResponse;
 import com.hyunn.capstone.dto.Response.ThreeDimensionResponse;
 import com.hyunn.capstone.service.ImageService;
 import com.hyunn.capstone.service.MeshyApiService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,23 +34,25 @@ public class ImageController {
   }
 
   @PostMapping("/text_to_3D/{keyWord}")
-  public ThreeDimensionCreateResponse textTo3D(
+  public ResponseEntity<ApiStandardResponse<ThreeDimensionCreateResponse>> textTo3D(
       @PathVariable String keyWord, @Valid @RequestBody ImageRequest imageRequest)
       throws JsonProcessingException {
-    return meshyApiService.textTo3D(imageRequest, keyWord);
+    ThreeDimensionCreateResponse threeDimensionCreateResponse =  meshyApiService.textTo3D(imageRequest, keyWord);
+    return ResponseEntity.ok(ApiStandardResponse.success(threeDimensionCreateResponse));
   }
 
   @GetMapping("/result/{previewResult}")
-  public ThreeDimensionResponse return3D(
+  public ResponseEntity<ApiStandardResponse<ThreeDimensionResponse>> return3D(
       @PathVariable String previewResult) {
-    return meshyApiService.return3D(previewResult);
+    ThreeDimensionResponse threeDimensionResponse = meshyApiService.return3D(previewResult);
+    return ResponseEntity.ok(ApiStandardResponse.success(threeDimensionResponse));
   }
 
   @PostMapping("/refine/{previewResult}")
-  public String refine3D(
-      @PathVariable String previewResult)
-      throws JsonProcessingException {
-    return meshyApiService.refine3D(previewResult);
+  public ResponseEntity<ApiStandardResponse<String>> refine3D(
+      @PathVariable String previewResult) throws JsonProcessingException {
+    String message = meshyApiService.refine3D(previewResult);
+    return ResponseEntity.ok(ApiStandardResponse.success(message));
   }
 
 }
