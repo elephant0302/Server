8e91675cad7435d8fa5c3c3bcb74c0cc36a99f5d
HyunTae Jo <hyuntae9912@naver.com>
2024-03-24 15:37:52 +0900

    1. 프로젝트 구조 변경 2. docker 및 git-action 수정 3. README 초안 작성

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM adoptopenjdk/openjdk17
+FROM openjdk:17-alpine
 
 COPY ./build/libs/CUK-CAPSTONE-0.0.1-SNAPSHOT.jar app.jar
 
--- src/main/java/com/hyunn/capstone/config/S3Config.java
+++ src/main/java/com/hyunn/capstone/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.hyunn.capstone.controller.config;
+package com.hyunn.capstone.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
