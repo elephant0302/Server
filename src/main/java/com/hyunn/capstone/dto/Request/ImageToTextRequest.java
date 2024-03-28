04f36a94a03ccf1740b2c4d6ef185618277b0da4
HyunTae Jo <hyuntae9912@naver.com>
2024-03-28 22:56:52 +0900

    Dto 수정에 따른 서비스 수정

---
+++ src/main/java/com/hyunn/capstone/dto/Request/ImageToTextRequest.java
@@ -0,0 +1,26 @@
+package com.hyunn.capstone.dto.Request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class ImageToTextRequest {
+
+  @Schema(type = "String", description = "성별", example = "female")
+  @NotBlank(message = "성별을 입력해주세요.")
+  private String gender;
+
+  @Schema(type = "String", description = "감정", example = "happy")
+  @NotBlank(message = "감정을 입력해주세요.")
+  private String emotion;
+
+  public ImageToTextRequest(String gender, String emotion) {
+    this.gender = gender;
+    this.emotion = emotion;
+  }
+
+  public static ImageToTextRequest create(String gender, String emotion) {
+    return new ImageToTextRequest( gender, emotion);
+  }
+}
