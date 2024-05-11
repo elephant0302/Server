3990ac0e93fd32073a2809bc4c7a4831514547e7
JunSoo <land8746@naver.com>
2024-05-11 16:45:29 +0900

    카카오페이 결제 승인 response 필드 생성 및 오류 수정, 오류 핸들러 생성

---
+++ src/main/java/com/hyunn/capstone/exception/UnauthorizedImageAccessException.java
@@ -0,0 +1,10 @@
+package com.hyunn.capstone.exception;
+
+public class UnauthorizedImageAccessException extends RuntimeException {
+
+  public UnauthorizedImageAccessException(String msg) {
+    super(msg);
+  }
+
+  public ErrorStatus toErrorCode() {return ErrorStatus.UNAUTHORIZED_IMAGE_ACCESS_EXCEPTION;}
+}
